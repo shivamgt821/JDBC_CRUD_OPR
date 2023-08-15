c865aba8670dd050e4e2675ac0a028a3113bf370
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-08-15 09:28:28 +0530

    Create TestDelete.java

---
+++ TestDelete.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class TestDelete {
+
+	public static void main(String[] args) {
+		Driver driver;// static method of drivermanager
+		try {
+			driver=new Driver();
+			DriverManager.registerDriver(driver);// register driver with drivemanager
+			String url = "jdbc:mysql://localhost:3306/wakanda";
+			String user = "root";
+			String password = "root";
+			Connection connection = DriverManager.getConnection(url,user,password);
+			System.out.println("Connected");
+			 Statement statement = connection.createStatement(); // statmenet is interface
+			String query1 = "DELETE FROM customer WHERE name = 'Messi'" ;
+			int res1 = statement.executeUpdate(query1);
+//			boolean  res1 = statement.execute(query1);
+			System.out.println(res1);
+			
+			connection.close();
+		}
+		catch(SQLException e1) {
+			e1.printStackTrace();
+		
+		}
+	}
+
+}
